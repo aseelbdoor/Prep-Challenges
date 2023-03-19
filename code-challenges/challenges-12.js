d8f621ea7e552b39b25d315ecd67b9cbee95280c
aseelbdoor <aseel.bdoor2001@gmail.com>
2023-03-19 12:05:53 +0000

    CC12

---
+++ code-challenges/challenges-12.js
@@ -0,0 +1,141 @@
+'use strict';
+// Important Note: 
+// Kindly use filter instead of for in all of your solutions
+
+// Resource:
+// Filter: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+// 
+// Given an array of values use filter to extract odd values.
+//  
+// Input:
+// [20, 54, 89, 41, 2, 31, 111, 15, 0, 31, 200] 
+// Output:
+// [89, 41, 31, 111, 31]
+// 
+
+const oddFiltration = (arr) => {
+    const result = arr.filter(x => x % 2 == 1);
+    return result;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// optional:
+// 
+// Given data of candidates return the candidates CVs that have more than 4 years of experience 
+// and works as JS developer 
+
+// Input:
+// var cvs = [
+//     {
+//         firstName: "Jason",
+//         LastName: "James",
+//         yearsOfExperience: 20,
+//         tech: "JS"
+//     },
+//     {
+//         firstName: "Shira",
+//         LastName: null,
+//         yearsOfExperience: 5,
+//         tech: ".Net"
+//     },
+//     {
+//         firstName: "Dmitri",
+//         LastName: "Akkerman",
+//         yearsOfExperience: 1,
+//         tech: "Python"
+//     },
+//     {
+//         firstName: "Isabella",
+//         LastName: null,
+//         yearsOfExperience: 4,
+//         tech: "Java"
+//     }
+// ]
+//
+// Output:
+// [
+//     {
+//         fullName: "Jason James",
+//         tech: "JS"
+//     }
+// ]
+
+//  Note that:
+//  1- Full name is first name + last name
+//  2- If one of the names is null dont add it to the full name
+
+const cvsFiltration = (arr) => {
+    function chan(obj) {
+        const newobj = new Object();
+        if (obj.yearsOfExperience > 4) {
+            if (obj.firstName !== null && obj.LastName !== null) {
+                return newobj;
+            }
+        }
+    }
+    const result = arr.filter(chan);
+    function convert(obj){
+        let newobj=new Object();
+        newobj.fullName=`${obj.firstName} ${obj.LastName}`;
+        newobj.tech=obj.tech;
+        return newobj;
+    }
+    const result2 = result.map(x=>
+        convert(x)
+        );
+    return result2;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// optional:
+// 
+// Given an array of words filter out the words that contain one of the vowels (a, e, i, o, u)
+
+// Input: ['car', 'boy', 'spy', 'building', 'why', 'dry' ]
+// Output: ['spy', 'why', 'dry']
+// 
+
+const vowelsFiltration = (arr) => {
+    const result = arr.filter(x =>
+        !(/[aeiou]/.test(x))
+    );
+    return result;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 04:
+// Optional:
+// 
+// An employer wants to compare between his employees skills, write a function that will extract the differences 
+// between the employees skills.
+
+// Input:
+// first_Employee = [C, C#, Java, Angular, PHP]
+// sec_Employee = [C#, .Net, JavaScript, React, Angular]
+
+// Output:
+// result = [C, Java, PHP, .Net, JavaScript, React]
+
+// Hint:
+// - the 'include' method can help you, read about it.
+
+const skillComparison = (arr1, arr2) => {
+    let result = arr1.filter(x =>!(arr2.includes(x)));
+    let result2=arr2.filter(x=>!(arr1.includes(x)));
+    result=result.concat(result2);
+    return result;
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+module.exports = { oddFiltration, cvsFiltration, vowelsFiltration, skillComparison };
+
